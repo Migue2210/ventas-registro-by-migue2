7547156e8637e2fef94fbc29bfb5e786da73f815
Migue2210 <pazhucatoursoperaciones@gmail.com>
2025-05-24 18:08:54 -0500

    App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem', fontFamily: 'Arial' }}>
+      <h1>Bienvenido, Migue 🐛</h1>
+      <p>Tu sistema de ventas está funcionando correctamente.</p>
+    </div>
+  );
+}
+
+export default App;
