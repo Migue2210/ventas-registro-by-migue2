e79e26114c521e2b61e64219d0ed7e8f2f2db614
Migue2210 <pazhucatoursoperaciones@gmail.com>
2025-05-24 17:57:40 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Reporte de Ventas</h1>
+      <p>¡Tu aplicación está funcionando correctamente!</p>
+    </div>
+  )
+}
